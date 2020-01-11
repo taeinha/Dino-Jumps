a9a8ab048d54acc6bc98eb5fc6ca7e051692bd69
taeinha <taeinha@gmail.com>
2020-01-10 22:49:39 -0500

    push files

--- src/game.js
+++ src/game.js
@@ -40,7 +40,7 @@ class Game {
     this.winner = false;
 
     this.background = new Image();
-    // this.background.src = 
+    this.background.src = "https://github.com/taeinha/js-climber/blob/master/src/images/Sky.png?raw=true";
     // retrieveHighScores(this.setHighScores.bind(this));
   }
 
